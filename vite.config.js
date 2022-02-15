1b638e48734e8416287b75a3f8bfaec488cab0b3
Marc Jansen <jansen@terrestris.de>
2022-02-15 10:44:48 +0100

    Fix prod build relative folders

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+// vite.config.js
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+const { readdirSync } = require('fs');
+
+const nested = readdirSync(__dirname, {encoding: 'utf8', withFileTypes: true})
+  .filter(file => file.name.endsWith('.html'))
+  .reduce((previous, current) => {
+    previous[current.name] = resolve(__dirname, current.name);
+    return previous;
+  }, {});
+
+module.exports = defineConfig({
+  base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        ...nested
+      }
+    }
+  }
+});
